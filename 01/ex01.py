35570d751e046a9fa74dd19c80f58f548b73d9ba
Roberto Padilha <robertocesardasilva@gmail.com>
2024-06-27 21:02:34 -0300

    Gabarito

--- 01/ex01.py
+++ 01/ex01.py
@@ -1,1 +1,6 @@
 def obter_acrescimos(tempo_jogo: int) -> tuple[int, int]:
+    acrescimos_total = tempo_jogo - 90
+    acrescimos_t1 = acrescimos_total // 3
+    acrescimos_t2 = acrescimos_t1 * 2
+
+    return acrescimos_t1, acrescimos_t2
--- 02/ex02.py
+++ 02/ex02.py
@@ -1,1 +1,14 @@
 def obter_lucro_maximo(precos: list[float]) -> float:
+    preco_minimo = float("inf")
+    lucro_maximo = 0
+
+    for preco in precos:
+        if preco < preco_minimo:
+            preco_minimo = preco
+
+        lucro = preco - preco_minimo
+
+        if lucro > lucro_maximo:
+            lucro_maximo = lucro
+
+    return lucro_maximo
